4f1a88a7b7536d4270bda8f330d770b6e9036698
zolfa <zolfa.gad@gmail.com>
2026-05-24 02:16:09 +0300

    update jwt authorize

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -2,7 +2,8 @@ import { verifyUserAuthToken } from "../utils/jwt.util.js";
 
 export const authorizationMiddleware = async (request, response, next) => {
   try {
-    const token = request.headers.authorization;
+    const authorize = request.headers[process.env.JWT_AUTH_HEADER].split(" ");
+    const token = authorize[1] ?? authorize[0];
     if (!token) {
       throw new Error("Token Not Found!!");
     }
